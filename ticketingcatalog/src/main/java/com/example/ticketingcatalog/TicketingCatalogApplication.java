181863627c323f4406bda1a9e51e8f54d22552b3
johancrp1 <yoquiarango@gmail.com>
2025-11-18 07:59:56 -0500

    HU2

--- ticketingcatalog/src/main/java/com/example/ticketingcatalog/TicketingCatalogApplication.java
+++ ticketingcatalog/src/main/java/com/example/ticketingcatalog/TicketingCatalogApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TicketingCatalogApplication {
 
     public static void main(String[] args) {
-        // Arranca la aplicación Spring Boot
+        // Arranca la aplicación Spring Boot.
         SpringApplication.run(TicketingCatalogApplication.class, args);
     }
 }
